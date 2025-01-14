0dba108b479e80e4de1c00815c82ab8ce11886c7
MahidunNobi <mdmahidunnobi@gmail.com>
2025-01-14 08:17:06 +0600

    Add: Logout, Redirect after login

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { Layout, theme } from "antd";
+import { Button, Layout, theme } from "antd";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import { useAppDispatch } from "../../redux/hooks";
+import { logout } from "../../redux/features/auth/authSlice";
+import { toast } from "sonner";
 
 const { Header, Content, Footer } = Layout;
 
 const MainLayout: React.FC = () => {
+  const dispatch = useAppDispatch();
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    toast("Logged Out");
+  };
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
       <Sidebar />
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Header>
+          <Button onClick={handleLogout}>Logout</Button>
+        </Header>
         <Content style={{ margin: "24px 16px 0" }}>
           <div
             style={{
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,11 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { verifyToken } from "../utils/verifyToken";
 import { useAppDispatch } from "../redux/hooks";
 import { setUser } from "../redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
   const { register, handleSubmit } = useForm({
@@ -14,13 +16,22 @@ const Login = () => {
   });
 
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-  const [login, { isError }] = useLoginMutation();
+  const [login] = useLoginMutation();
 
-  const onSubmit = async (userInfo) => {
-    const res = await login(userInfo).unwrap();
-    const user = verifyToken(res.data.accessToken);
-    dispatch(setUser({ user, token: res.data.accessToken }));
+  const onSubmit = async (userInfo: FieldValues) => {
+    const toastId = toast.loading("Logging in...");
+    try {
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.accessToken);
+      dispatch(setUser({ user, token: res.data.accessToken }));
+      toast.success("Logged in", { id: toastId });
+      navigate(`/admin/dashboard`);
+    } catch (error) {
+      toast.error("Something went wrong", { id: toastId });
+      console.log(error);
+    }
   };
 
   return (
