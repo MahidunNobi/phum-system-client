cd6687a575b6cb8612d5c220971148b07c0dd80d
MahidunNobi <mdmahidunnobi@gmail.com>
2025-01-16 10:05:31 +0600

    Revaladating when the access token is expired

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -16,6 +16,19 @@ const baseQuery = fetchBaseQuery({
 const baseQueryWithRefreashToken = async (args, api, extraOptions) => {
   const result = await baseQuery(args, api, extraOptions);
   console.log(result);
+
+  // Handling unauthorized access
+  if (result.error?.status === 401) {
+    console.log("Revaladading tokens");
+
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+    const data = await res.json();
+    console.log(data);
+  }
+  return result;
 };
 
 export const baseApi = createApi({
