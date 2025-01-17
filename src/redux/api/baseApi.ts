adcbdf5a13f6e2f2ae80d591dfab053e2601ad63
MahidunNobi <mdmahidunnobi@gmail.com>
2025-01-17 22:03:35 +0600

    Getting new token using refreshToken

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -14,8 +15,7 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithRefreashToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
-  console.log(result);
+  let result = await baseQuery(args, api, extraOptions);
 
   // Handling unauthorized access
   if (result.error?.status === 401) {
@@ -26,7 +26,9 @@ const baseQueryWithRefreashToken = async (args, api, extraOptions) => {
       credentials: "include",
     });
     const data = await res.json();
-    console.log(data);
+    const user = (api.getState() as RootState).auth.user;
+    api.dispatch(setUser({ user, token: data.data.accessToken }));
+    result = await baseQuery(args, api, extraOptions);
   }
   return result;
 };
